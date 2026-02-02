6761ccfcd1fa80f4e9d9beb4c0f31ac91ae1292b
gnngugi <gnngugi@safaricom.co.ke>
2026-02-02 14:43:59 +0300

    updated the login tests

--- Pages/LoginPage.py
+++ Pages/LoginPage.py
@@ -6,20 +6,23 @@ class LoginPage(WebActions):
     def __init__(self, driver):
         super().__init__(driver)
         
-        
-    def login_as_standard_user(self, username, password):
+    def mainloginsteps(self,username,password):
         self.type("LoginPageUsernameinputField_XPATH", username)
         self.type("LoginPagePasswordinputField_XPATH", password)
         self.click("LoginPageloginButton_XPATH")
+        
+        
+        
+    def login_as_standard_user(self):
+        self.mainloginsteps("standard_user","secret_sauce")
 
     def get_inventory_page_title(self):
         return self._get_element("InventoryPageTitle_XPATH").text
         
         
-    def login_as_lockedout_user(self, username,password):
-        self.type("LoginPageUsernameinputField_XPATH", username)
-        self.type("LoginPagePasswordinputField_XPATH", password)
-        self.click("LoginPageloginButton_XPATH")
+    def login_as_lockedout_user(self):
+        self.mainloginsteps("locked_out_user","secret_sauce")
+        
         
         
     def get_lockedoutuservalidation(self):
